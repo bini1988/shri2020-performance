148faa522a5c6a8bc234e7c40e9533b736069632
Nikolai Karastelev <n.karastelev@credinform.ru>
2020-04-08 10:46:23 +0300

    Remove owl carousel js

--- js/main.js
+++ js/main.js
@@ -8,11 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-$(document).ready(function(){
-    var carousel = $("#carousel");
-    // carousel.owlCarousel();
-});
-
 document.addEventListener("DOMContentLoaded", function () {
     const buttonsContainer = document.querySelector(".buttons-wrap");
     const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
